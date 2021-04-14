56225c4c5fa436a432e27f557202b1e776f83ba8
AadityaSingh360 <56865898+AadityaSingh360@users.noreply.github.com>
2021-04-14 19:41:59 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:ombre_test/DataController.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
